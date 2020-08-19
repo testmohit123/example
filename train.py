25efa3edde969dc3fbbeb2b73b320b711e7b63e9
Pawar <850047756@genpact.com>
2020-08-20 02:25:52 +0530

    pushing files

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
-import pandas as pd 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import accuracy_score
 import numpy as np
+
 # Set random seed
 seed = 42
 
@@ -16,7 +17,7 @@ seed = 42
 df = pd.read_csv("wine_quality.csv")
 
 # Split into train and test sections
-X = df.drop('quality',axis = 1)
+X = df.drop('quality', axis=1)
 y = df.quality
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
 
@@ -30,13 +31,11 @@ regr.fit(X_train, y_train)
 
 # Report training set score
 predictions = regr.predict(X_test)
-train_score = accuracy_score(predictions,y_test) * 100
+train_score = accuracy_score(predictions, y_test) * 100
 
 # Write scores to a file
-with open("metrics.txt", 'w') as outfile:
-        outfile.write("Accuracy of the Model is: %2.1f%%\n" % train_score)
-        
-
+with open("metrics.json", 'w') as outfile:
+    outfile.write("Accuracy of the Model is: %2.1f%%\n" % train_score)
 
 ##########################################
 ##### PLOT FEATURE IMPORTANCE ############
@@ -44,44 +43,30 @@ with open("metrics.txt", 'w') as outfile:
 # Calculate feature importance in random forest
 importances = regr.feature_importances_
 labels = df.columns
-feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
-feature_df = feature_df.sort_values(by='importance', ascending=False,)
+feature_df = pd.DataFrame(list(zip(labels, importances)), columns=["feature", "importance"])
+feature_df = feature_df.sort_values(by='importance', ascending=False, )
 
 # image formatting
-axis_fs = 18 #fontsize
-title_fs = 22 #fontsize
+axis_fs = 18  # fontsize
+title_fs = 22  # fontsize
 sns.set(style="whitegrid")
 
 ax = sns.barplot(x="importance", y="feature", data=feature_df)
-ax.set_xlabel('Importance',fontsize = axis_fs) 
-ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
-ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
+ax.set_xlabel('Importance', fontsize=axis_fs)
+ax.set_ylabel('Feature', fontsize=axis_fs)  # ylabel
+ax.set_title('Random forest\nfeature importance', fontsize=title_fs)
 
 plt.tight_layout()
-plt.savefig("feature_importance.png",dpi=120)
+plt.savefig("feature_importance.png", dpi=120)
 plt.close()
 
-
-
-##########################################
-############ PLOT RESIDUALS  #############
+###############################################
+############ PLOT CONFUSION MATRIX #############
 ##########################################
 
-#y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
-#y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
-#res_df = pd.DataFrame(list(zip(y_test,predictions)), columns = ["true","pred"])
-
-#ax = sns.barpl(x="true", y="pred",data=res_df)
-#ax.set_aspect('equal')
-#ax.set_xlabel('True wine quality',fontsize = axis_fs) 
-#ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
-#ax.set_title('Residuals', fontsize = title_fs)
-
-# Make it pretty- square aspect ratio
-#ax.plot([1, 10], [1, 10], 'black', linewidth=1)
-#plt.ylim((2.5,8.5))
-#plt.xlim((2.5,8.5))
-
-#plt.tight_layout()
-#plt.savefig("residuals.png",dpi=120) 
+from sklearn.metrics import plot_confusion_matrix
+plot_confusion_matrix(regr,X_test,y_test)
+plt.tight_layout()
+plt.savefig("confusion_matrix.png",dpi=120)
+plt.close()
 
