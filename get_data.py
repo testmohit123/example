7c5f1fa4c538a2e01cab3ddfcb0202c3bc1c8bec
Pawar <850047756@genpact.com>
2020-08-21 14:55:34 +0530

    without dvc

--- get_data.py
+++ get_data.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import io
 import requests
-import os
-import numpy as np
+
 
 url = "https://raw.githubusercontent.com/elleobrien/wine/master/wine_quality.csv"
 s = requests.get(url).content
 ds = pd.read_csv(io.StringIO(s.decode('utf-8')),sep=',')
 
 # Save it
-ds.to_csv('wine_quality.csv')
+ds.to_csv('wine_quality.csv',index=False)
 
 
